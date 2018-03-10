68173c8449ad05fcd0e55ee8baaec7f49b70d50e
yutian zhang <yutianzhang@yutians-MacBook-Pro.local>
2018-03-09 21:40:19 -0800

    update

--- classifierHelp.py
+++ classifierHelp.py
@@ -10,10 +10,11 @@ def evaluation(predictions, test_y):
     for i in range(len(predictions)):
         if predictions[i]!= test_y[i]:
             err_cnt+=1
+        print (predictions[i])
     print ('Accuracy', 1-err_cnt/float(len(predictions)))
 
 
-def generateData():
+def generateData(method = "slides window", win_size = 3, step = 2):
     path = "clean_data"
     files = os.listdir(path)
     dfs = []
@@ -21,7 +22,7 @@ def generateData():
         if file.endswith(".csv"):
             df = pd.read_csv(path + '/' + file, index_col=0)
             dfs.append(df)
-    data = dp.preprocess(dfs)
+    data = dp.preprocess(dfs, method, win_size, step)
     y = data['label'].values
     x = data.drop(df.columns[-1], axis=1).values
     return x, y
--- decisionTree.py
+++ decisionTree.py
@@ -1,20 +1,21 @@
 from sklearn import svm
+from sklearn import tree 
 from sklearn.cross_validation import train_test_split
 import classifierHelp as help
 
-def SVM():
+def decisionTree():
     x, y  = help.generateData()
     train_x, test_x, train_y, test_y = train_test_split(x, y, test_size = 0.33, random_state = 42)
 
-    ## use SVM to classify dataset
+    ## use decisionTree to classify dataset
     # rbf is the default kernal: Gaussin kernal, ovr means one vs rest
-    classifier=svm.SVC(C=0.8, kernel='rbf', gamma=20, decision_function_shape='ovr')
+    classifier = tree.DecisionTreeClassifier(criterion='gini')
     classifier.fit(train_x, train_y)
     predictions=classifier.predict(test_x)
     help.evaluation(predictions, test_y)
 
 def main():
-    SVM()
+    decisionTree()
 
 if __name__ == '__main__':
     main()
