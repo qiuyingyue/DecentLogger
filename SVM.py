9b0fb34b52efe6d30fd14b913edf5c07626f9049
yutian zhang <yutianzhang@yutians-MacBook-Pro.local>
2018-03-09 21:46:55 -0800

    update

--- SVM.py
+++ SVM.py
@@ -7,8 +7,8 @@ def SVM():
     train_x, test_x, train_y, test_y = train_test_split(x, y, test_size = 0.33, random_state = 42)
 
     ## use SVM to classify dataset
-    # rbf is the default kernal: Gaussin kernal, ovr means one vs rest
-    classifier=svm.SVC(C=0.8, kernel='rbf', gamma=20, decision_function_shape='ovr')
+    # poly is the best kernal for this case, ovr means one vs rest
+    classifier=svm.SVC(C=0.8, kernel='poly', gamma=20, decision_function_shape='ovr')
     classifier.fit(train_x, train_y)
     predictions=classifier.predict(test_x)
     help.evaluation(predictions, test_y)
